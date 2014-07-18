f3256eb9e1f0a17dddcca749579476d3758583b2
daimar_a <anis.daimar@epitech.eu>
2014-07-18 03:46:34 +0200

    prototype

---
+++ app.js
@@ -0,0 +1,19 @@
+var api = require('./api/');
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var port = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static('public'));
+
+app.use('/api', api);
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.listen(port);
+console.log('Welcome to twitwall !\nPlease go to http://localhost:' + port + ' to start.');
---
+++ public/js/tweets.js
@@ -0,0 +1,15 @@
+angular.module('tweetWall', [])
+  .controller('TweetController', ['$scope', '$http', function($scope, $http) {
+    $scope.tweets = [];
+    var socket = io.connect('http://localhost:8000');
+    socket.on('tweet', function(data){
+      $scope.tweets.push(data);
+      console.log(data);
+      $scope.$apply();
+    });
+
+    $scope.changeHash = function(){
+      $scope.tweets = [];
+      $http.get('/api/' + $scope.hashtag);
+    };
+  }]);
