49c3ad797aa60bcf88b13657bc9b90078b587b16
daimar_a <anis.daimar@epitech.eu>
2014-07-18 15:50:16 +0200

    multi user sockets correction

--- public/js/tweets.js
+++ public/js/tweets.js
@@ -1,16 +1,25 @@
 angular.module('tweetWall', [])
   .controller('TweetController', ['$scope', '$http', function($scope, $http) {
     $scope.tweets = [];
-    
-    var socket = io.connect('http://localhost:8000/gaza');
-    socket.on('tweet', function(data){
+    var socket = null;
+
+    onTweet = function(data){
       $scope.tweets.push(data);
       console.log(data);
       $scope.$apply();
-    });
+    };
 
     $scope.changeHash = function(){
+      var hashtag = $scope.hashtag.toLowerCase();
+
       $scope.tweets = [];
-      $http.get('/api/' + $scope.hashtag);
+      $http.get('/api/' + hashtag).success(function(){  
+        if (socket) {
+          socket.disconnect();
+          delete socket;
+        }
+        socket = io.connect('http://localhost:8000/' + hashtag);
+        socket.on('tweet', onTweet);
+      });
     };
   }]);
